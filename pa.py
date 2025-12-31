832d7640cef837aa0a15548df938049c304140dd
Abrar Rakin <rakinabrar151@gmail.com>
2025-12-31 21:50:05 +0600

    Initialize and Function to make the assistant speak

--- pa.py
+++ pa.py
@@ -4,3 +4,11 @@ import pyttsx3
 import datetime
 import webbrowser
 import os
+
+# Initialize the speech engine for text-to-speech
+engine = pyttsx3.init()
+
+# Function to make the assistant speak
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
